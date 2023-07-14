3362b8206c6f30ec50721f012b66b6a7a5858647
Robert Marrett <Robmmar90@gmail.com>
2023-07-14 16:44:52 -0600

    initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,21 @@ config();
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Deck from "./models/Deck";
+import cors from "cors";
 
-const port: Number = 5000;
+const port: Number = parseInt(process.env.PORT!);
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
+});
 
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new Deck({
